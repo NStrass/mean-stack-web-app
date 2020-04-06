ee5978b11898c67386e2c55250b8fcfaa3152f95
NStrass <38595571+NStrass@users.noreply.github.com>
2020-04-06 15:25:01 -0300

    Create index.controller.js

---
+++ app/controle-estoque/index.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+   'use strict';
+
+   angular
+      .module('app')
+      .controller('ControleEstoque.IndexController', Controller);
+
+   function Controller(ControleEstoqueService, FlashService, $location) {
+      const vm = this;
+
+      vm.movimentacoes = null;
+      vm.getAll = getAll;
+      vm.irParaEntradas = irParaEntradas;
+      vm.irParaSaidas = irParaSaidas;
+
+      getAll();
+
+      function getAll() {
+         ControleEstoqueService.getAll()
+            .then((res) => {
+               vm.movimentacoes = res;
+            })
+            .catch((err) => {
+               FlashService.Error(err);
+            });
+      }
+
+      function irParaSaidas() {
+         $location.path('/controle-estoque/nova-saida');
+      }
+
+      function irParaEntradas() {
+         $location.path('/controle-estoque/nova-entrada');
+      }
+   }
+})(); 
