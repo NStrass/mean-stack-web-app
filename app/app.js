ee27ecbc8b4ccda2403c13ea5824e69975953ce1
NStrass <38595571+NStrass@users.noreply.github.com>
2020-04-06 15:14:45 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-﻿(function () {
+(function () {
     'use strict';
 
     angular
@@ -24,13 +24,42 @@
                 controller: 'Account.IndexController',
                 controllerAs: 'vm',
                 data: { activeTab: 'account' }
-            });
+            })
+            .state('questions', {
+                url: '/questions',
+                templateUrl: 'questions/index.html',
+                controller: 'Questions.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'questions' }
+            })
+            .state('controle-estoque', {
+               url: '/controle-estoque',
+               templateUrl: 'controle-estoque/index.html',
+               controller: 'ControleEstoque.IndexController',
+               controllerAs: 'vm',
+               data: { activeTab: 'controle-estoque' }
+           })
+            .state('nova-entrada', {
+               url: '/controle-estoque/nova-entrada',
+               templateUrl: 'controle-estoque/entrada/index.html',
+               controller: 'ControleEstoqueEntrada.IndexController',
+               controllerAs: 'vm',
+               data: { activeTab: 'controle-estoque' }
+           })
+            .state('nova-saida', {
+               url: '/controle-estoque/nova-saida',
+               templateUrl: 'controle-estoque/saida/index.html',
+               controller: 'ControleEstoqueSaida.IndexController',
+               controllerAs: 'vm',
+               data: { activeTab: 'controle-estoque' }
+           });
     }
 
     function run($http, $rootScope, $window) {
         // get JWT token from server
         $.get('/app/token', function (token) {
             // add JWT token as default auth header
+            console.log(token)
             $http.defaults.headers.common['Authorization'] = 'Bearer ' + token;
             // update active tab on state change
             $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
